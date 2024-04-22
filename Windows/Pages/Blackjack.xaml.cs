d20ec477fae8ac1b3674061bacd213b98fdc24b3
11344 <11344@16S235>
2024-04-22 14:23:12 +0200

    Added Windows and Pages

---
+++ Windows/Pages/Blackjack.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CasinoRoyale.Windows.Pages
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Blackjack.xaml
+    /// </summary>
+    public partial class Blackjack : UserControl
+    {
+        private static MainWindow window;
+        public Blackjack(MainWindow win)
+        {
+            window = win;
+            InitializeComponent();
+        }
+    }
+}
---
+++ Windows/Pages/Poker.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CasinoRoyale.Windows.Pages
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Poker.xaml
+    /// </summary>
+    public partial class Poker : UserControl
+    {
+        private static MainWindow window;
+        public Poker(MainWindow win)
+        {
+            window = win;
+            InitializeComponent();
+        }
+    }
+}
