101b69ac764eaca454cd5d7a3cd18e6d560ca26b
uczen <uczen@DESKTOP-87QG7AA>
2024-04-29 10:32:43 +0200

    Changes to class CalculationsBlackjack

---
+++ classes/Calculations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoRoyale.classes
+{
+    abstract class Calculations
+    {
+        protected int[] deck = Array.Empty<int>();
+        protected List<int> casinoCards = new();
+        protected List<int> userCards = new();
+        protected int bet = 0;
+
+        // Base function
+        protected void SetCards(int firstCard, int secondCard)
+        {
+            SetCard(firstCard);
+            SetCard(secondCard);
+        }
+
+        // Adding new Card
+        public void SetCard(int newCard)
+        {
+            casinoCards.Add(newCard);
+            deck[newCard - 2]--;
+        }
+
+        // Changing Card Array
+        public void SetCards(List<int> usersCard)
+        {
+            for (int i = 0; i < deck.Length; i++)
+                deck[i] = usersCard[i];
+            foreach (int card in usersCard)
+                userCards.Add(card);
+        }
+
+        public void SetBet(int bet)
+        {
+            this.bet = bet;
+        }
+    }
+}
---
+++ classes/CalculationsBlackjack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CasinoRoyale.classes
+{
+    internal class CalculationsBlackjack : Calculations
+    {
+        protected new int[] deck = { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 };
+        protected int userScore = 0;
+        protected int casinoScore = 0;
+
+        // Constructors
+        public CalculationsBlackjack() { }
+        public CalculationsBlackjack(int firstCard, int secondCard, List<int> playersCards)
+        {
+            SetCards(firstCard, secondCard);
+            SetCards(playersCards);
+            SetScore(playersCards, false);
+        }
+
+        private void SetScore(List<int> cards, Boolean casino)
+        {
+            foreach (int card in cards)
+                if (casino)
+                    casinoScore += card;
+                else
+                    userScore += card;
+        }
+
+        public Boolean Probability()
+        {
+            SetScore(casinoCards, true);
+            if (casinoScore < 21)
+            {
+                int difference = 21 - casinoScore;
+                int possibleCards = 0, allPossibleCards = 0;
+                for (int i = 2; i < 15; i++)
+                    if (difference - i > -1)
+                        possibleCards += deck[i - 2];
+                foreach (int card in deck)
+                    allPossibleCards += card;
+
+                if (possibleCards / allPossibleCards >= 0.25)
+                    return true;
+            }
+            return false;
+        }
+
+        public int CheckWin()
+        {
+            // codes || 0 - casino win | 1 - user win | 2 - draw | 3 - blackjack ||
+            if (casinoScore <= userScore)
+                if (userCards.Contains(14) && (userCards.Contains(13) || userCards.Contains(12) || userCards.Contains(11) || userCards.Contains(10)))
+                    return 3;
+                else
+                    if (casinoScore == userScore)
+                    return 2;
+                else
+                    return 1;
+            else
+                return 0;
+        }
+    }
+}
