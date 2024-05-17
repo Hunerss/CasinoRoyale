b0e578c24334b770d0f875749266f6567555d0d7
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-05-17 19:56:48 +0200

    Added new modules to blackjack

---
+++ windows/pages/CardModel.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CasinoRoyale.windows.pages
+{
+    /// <summary>
+    /// Logika interakcji dla klasy CardModel.xaml
+    /// </summary>
+    public partial class CardModel : UserControl
+    {
+        public Point TargetPositon;
+        public Point StartPosition;
+        public double TimeToTarget;
+
+        public CardModel()
+        {
+            InitializeComponent();
+        }
+
+        public void SetPosition(double x, double y)
+        {
+            SetValue(Window.LeftProperty, x - MainGrid.ActualWidth / 2);
+            SetValue(Window.TopProperty, y - MainGrid.ActualHeight / 2);
+        }
+
+        public Point GetPosition()
+        {
+            Point p = new Point();
+            p.X = (double)GetValue(Window.LeftProperty) + MainGrid.ActualWidth / 2;
+            p.Y = (double)GetValue(Window.TopProperty) + MainGrid.ActualHeight / 2;
+            return p;
+        }
+
+
+        public void Update()
+        {
+            TimeToTarget += 0.02;
+            if (TimeToTarget > 1)
+                TimeToTarget = 1;
+
+            SetPosition(Lerp(StartPosition.X, TargetPositon.X, TimeToTarget), Lerp(StartPosition.Y, TargetPositon.Y, TimeToTarget));
+        }
+
+        public static double Lerp(double a, double b, double t)
+        {
+            return a * (1 - t) + b * t;
+        }
+
+        public void SetTargetPosition(Point target)
+        {
+            StartPosition = GetPosition();
+            TimeToTarget = 0;
+            TargetPositon = target;
+        }
+
+        private void MainGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetTargetPosition(new Point(GetPosition().X, GetPosition().Y + 50));
+        }
+    }
+}
