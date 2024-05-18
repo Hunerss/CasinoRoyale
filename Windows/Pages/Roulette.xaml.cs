e71b1eebb4ab1de86bd45eba38281e24ddb11a10
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-05-18 23:31:23 +0200

    Work on roulette

--- Windows/Pages/Roulette.xaml.cs
+++ Windows/Pages/Roulette.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace CasinoRoyale.Windows.Pages
 {
@@ -21,10 +22,45 @@ namespace CasinoRoyale.Windows.Pages
     public partial class Roulette : UserControl
     {
         private static MainWindow window;
+        private DispatcherTimer rotationTimer;
+        private RotateTransform rotateTransform;
+        private int currentAngle;
+        private int totalRotations;
         public Roulette(MainWindow win)
         {
             window = win;
             InitializeComponent();
+
+            rotateTransform = new RotateTransform
+            {
+                CenterX = 150,
+                CenterY = 150 
+            };
+            wheel.RenderTransform = rotateTransform;
+
+            rotationTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(100)
+            };
+            rotationTimer.Tick += RotateWheel;
+        }
+
+        private void RotateWheel(object sender, EventArgs e)
+        {
+            currentAngle += 15;
+            if (currentAngle >= 360)
+            {
+                currentAngle = 0;
+                totalRotations++;
+            }
+
+            rotateTransform.Angle = currentAngle;
+
+            if (totalRotations >= 3)
+            {
+                rotationTimer.Stop();
+                totalRotations = 0;
+            }
         }
 
         private void Navigation(object sender, RoutedEventArgs e)
@@ -32,6 +68,12 @@ namespace CasinoRoyale.Windows.Pages
             string btnName = ((Button)sender).Name[4].ToString();
             if (btnName == "0")
                 window.frame.NavigationService.Navigate(new Welcome(window));
+            if (btnName == "1")
+            {
+                currentAngle = 0;
+                totalRotations = 0;
+                rotationTimer.Start();
+            }
             else
                 Console.WriteLine("Blackjack - error log - Navigation button number to bit - " + btnName);
         }
