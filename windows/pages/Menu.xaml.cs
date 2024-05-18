b48e795ebcbbe15f48a0e9654dc81a425cb1dec9
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-05-19 00:13:11 +0200

    Added Menu as database conection service base

---
+++ windows/pages/Menu.xaml.cs
@@ -0,0 +1,43 @@
+﻿using CasinoRoyale.Windows.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CasinoRoyale.windows.pages
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Menu.xaml
+    /// </summary>
+    public partial class Menu : UserControl
+    {
+        MainWindow window;
+        public Menu(MainWindow win)
+        {
+            this.window = win;
+            InitializeComponent();
+        }
+
+        private void Navigation(object sender, RoutedEventArgs e)
+        {
+            string btnName = ((Button)sender).Name[4].ToString();
+            if (btnName == "1")
+                window.frame.NavigationService.Navigate(new Login(window));
+            else if (btnName == "2")
+                window.frame.NavigationService.Navigate(new Registration(window));
+            else
+                Console.WriteLine("Welcome - error log - Navigation button number to bit - " + btnName);
+        }
+    }
+}
