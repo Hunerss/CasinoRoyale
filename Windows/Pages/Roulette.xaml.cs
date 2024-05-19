9dfc844170168ae183952716e3f88a298f72ca84
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-05-19 19:16:05 +0200

    Upgradeg and finalised Roulette

--- Windows/Pages/Roulette.xaml.cs
+++ Windows/Pages/Roulette.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CasinoRoyale.classes;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -15,6 +18,7 @@ namespace CasinoRoyale.Windows.Pages
         private DispatcherTimer rotationTimer;
         private RotateTransform rotateTransform;
         private Random rnd = new();
+        private RouletteOperations ro;
 
         private int currentAngle;
         private int totalRotations;
@@ -27,10 +31,15 @@ namespace CasinoRoyale.Windows.Pages
             0, 26, 3, 35, 12, 28, 7, 29, 18, 22, 9, 31, 14, 20, 1, 33, 16, 24, 5, 10, 23, 8, 30, 11, 36, 13, 27, 6, 34, 17, 25, 2, 21, 4, 19, 15, 32
         };
 
+        private List<int> bettedNumbers;
+        private List<int> bettedNumbersBets;
+
         public Roulette(MainWindow win)
         {
-            window = win;
             InitializeComponent();
+            bettedNumbers = new List<int>();
+            bettedNumbersBets = new List<int>();
+            window = win;
 
             rotateTransform = new RotateTransform
             {
@@ -58,13 +67,6 @@ namespace CasinoRoyale.Windows.Pages
                 totalRotations++;
             }
 
-            /*
-            6 - 10
-            25 - 8/30
-            35 - 21
-             
-             */
-
             rotateTransform.Angle = currentAngle;
 
             if (currentAngle == 0)
@@ -84,7 +86,14 @@ namespace CasinoRoyale.Windows.Pages
                 rotationTimer.Stop();
                 totalRotations = 0;
                 int ballPosition = GetBallPosition();
-                MessageBox.Show("Ball landed on: " + ballPosition);
+                ro.SetFile(ballPosition);
+                int wonBet = ro.Game();
+
+                if (wonBet > 0)
+                    MessageBox.Show("Ball landed on: " + ballPosition + Environment.NewLine + "You won " + wonBet + " form your bets");
+                else
+                    MessageBox.Show("Ball landed on: " + ballPosition + Environment.NewLine + "You lost all your bets");
+                Clear();
             }
             else
             {
@@ -118,14 +127,153 @@ namespace CasinoRoyale.Windows.Pages
                 window.frame.NavigationService.Navigate(new Welcome(window));
             if (btnName == "1")
             {
-                ballAngle = rnd.Next(0, 360);
-                maxRotations = rnd.Next(3, 7);
-                rotationTimer.Interval = TimeSpan.FromMilliseconds(30);
-                totalRotations = 0;
-                rotationTimer.Start();
+
+                string setBetCheck = setBet.Text;
+                string allBetCheck = allBet.Text;
+                if (CheckBet(setBetCheck) && CheckBet(allBetCheck))
+                {
+                    setBet.IsEnabled = false;
+                    ro = new(bettedNumbers, bettedNumbersBets);
+                    ballAngle = rnd.Next(0, 360);
+                    maxRotations = rnd.Next(3, 7);
+                    rotationTimer.Interval = TimeSpan.FromMilliseconds(30);
+                    totalRotations = 0;
+                    rotationTimer.Start();
+                }
+
             }
             else
                 Console.WriteLine("Blackjack - error log - Navigation button number to bit - " + btnName);
+        }
+
+        private void BetOperations(object sender, RoutedEventArgs e)
+        {
+            string betCheck = setBet.Text;
+            if (CheckBet(betCheck))
+            {
+                int bet = Convert.ToInt32(betCheck);
+                string btnName = ((Button)sender).Name[4..];
+
+                Console.WriteLine($"Button Name: {btnName}");
+
+                if (int.TryParse(btnName, out int btnNumber))
+                {
+                    Console.WriteLine($"Parsed Button Number: {btnNumber}");
+
+                    if (btnNumber >= 0 && btnNumber <= 45)
+                    {
+                        btn_0.IsEnabled = false;
+                        bettedNumbers.Add(btnNumber);
+                        bettedNumbersBets.Add(bet);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Button number is out of range (0-36)");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Failed to parse button name");
+                }
+
+                allBet.Text = (Convert.ToInt32(allBet.Text) + bet).ToString();
+                MessageBox.Show("Betted " + bet + " on " + GetBetName(btnName));
+
+                DisableButton("bet_" + btnName);
+            }
+            else
+            {
+                MessageBox.Show("Enter correct bet");
+            }
+        }
+
+        private void DisableButton(string buttonName)
+        {
+            Button button = GetType().GetField(buttonName, BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(this) as Button;
+            if (button != null)
+                button.IsEnabled = false;
+        }
+
+        private static string GetBetName(string btn_name)
+        {
+            return btn_name switch
+            {
+                "37" => "first 12",
+                "38" => "second 12",
+                "39" => "third 12",
+                "40" => "1 to 18",
+                "41" => "even",
+                "42" => "red",
+                "43" => "black",
+                "44" => "odd",
+                "45" => "18 to 36",
+                _ => btn_name,
+            };
+        }
+
+        private static Boolean CheckBet(string betToCheck)
+        {
+            Boolean check = true;
+            foreach (char character in betToCheck)
+            {
+                if (!char.IsNumber(character))
+                    check = false;
+            }
+            Console.WriteLine(check);
+            return check;
+        }
+
+        private void Clear()
+        {
+            bet_0.IsEnabled = true;
+            bet_1.IsEnabled = true;
+            bet_2.IsEnabled = true;
+            bet_3.IsEnabled = true;
+            bet_4.IsEnabled = true;
+            bet_5.IsEnabled = true;
+            bet_6.IsEnabled = true;
+            bet_7.IsEnabled = true;
+            bet_8.IsEnabled = true;
+            bet_9.IsEnabled = true;
+            bet_10.IsEnabled = true;
+            bet_11.IsEnabled = true;
+            bet_12.IsEnabled = true;
+            bet_13.IsEnabled = true;
+            bet_14.IsEnabled = true;
+            bet_15.IsEnabled = true;
+            bet_16.IsEnabled = true;
+            bet_17.IsEnabled = true;
+            bet_18.IsEnabled = true;
+            bet_19.IsEnabled = true;
+            bet_20.IsEnabled = true;
+            bet_21.IsEnabled = true;
+            bet_22.IsEnabled = true;
+            bet_23.IsEnabled = true;
+            bet_24.IsEnabled = true;
+            bet_25.IsEnabled = true;
+            bet_26.IsEnabled = true;
+            bet_27.IsEnabled = true;
+            bet_28.IsEnabled = true;
+            bet_29.IsEnabled = true;
+            bet_30.IsEnabled = true;
+            bet_31.IsEnabled = true;
+            bet_32.IsEnabled = true;
+            bet_33.IsEnabled = true;
+            bet_34.IsEnabled = true;
+            bet_35.IsEnabled = true;
+            bet_36.IsEnabled = true;
+            bet_37.IsEnabled = true;
+            bet_38.IsEnabled = true;
+            bet_39.IsEnabled = true;
+            bet_40.IsEnabled = true;
+            bet_41.IsEnabled = true;
+            bet_42.IsEnabled = true;
+            bet_43.IsEnabled = true;
+            bet_44.IsEnabled = true;
+            bet_45.IsEnabled = true;
+            allBet.Text = "0";
+            setBet.IsEnabled = true;
+            btn_0.IsEnabled = true;
         }
     }
 }
--- classes/RouletteOperations.cs
+++ classes/RouletteOperations.cs
@@ -1,12 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace CasinoRoyale.classes
 {
     class RouletteOperations
     {
+        private List<int> bettedFiles;
+        private List<int> bettedFilesBets;
+        private readonly int[] wheelNumbers = new int[]
+        {
+            0, 26, 3, 35, 12, 28, 7, 29, 18, 22, 9, 31, 14, 20, 1, 33, 16, 24, 5, 10, 23, 8, 30, 11, 36, 13, 27, 6, 34, 17, 25, 2, 21, 4, 19, 15, 32
+        };
+        private int file;
+        private int win;
+
+        private static readonly Dictionary<int, (string BetType, int Multiplier)> betTypes = new()
+        {
+            { 37, ("First 12", 2) },
+            { 38, ("Second 12", 2) },
+            { 39, ("Third 12", 2) },
+            { 40, ("1 to 18", 1) },
+            { 41, ("Even", 1) },
+            { 42, ("Red", 1) },
+            { 43, ("Black", 1) },
+            { 44, ("Odd", 1) },
+            { 45, ("19 to 36", 1) }
+        };
+
+        private static readonly HashSet<int> redNumbers = new()
+        {
+            1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36
+        };
+
+        private static readonly HashSet<int> blackNumbers = new()
+        {
+            2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35
+        };
+
+        public RouletteOperations(List<int> bettedFiles, List<int> bettedFilesBets)
+        {
+            this.bettedFiles = bettedFiles;
+            this.bettedFilesBets = bettedFilesBets;
+        }
+
+        public void SetFile(int number)
+        {
+            file = number;
+        }
+
+        public int Game()
+        {
+            win = 0;  // Reset win at the start of the game
+
+            for (int i = 0; i < bettedFiles.Count; i++)
+            {
+                int bet = bettedFiles[i];
+                int betAmount = bettedFilesBets[i];
+
+                switch (bet)
+                {
+                    case < 37:
+                        if (bet == file)
+                            win += betAmount * 35;
+                        break;
+                    case 37:
+                        if (file < 13)
+                            win += betAmount * 2;
+                        break;
+                    case 38:
+                        if (file > 12 && file < 25)
+                            win += betAmount * 2;
+                        break;
+                    case 39:
+                        if (file > 24)
+                            win += betAmount * 2;
+                        break;
+                    case 40:
+                        if (file >= 1 && file <= 18)
+                            win += betAmount * 1;
+                        break;
+                    case 41:
+                        if (file % 2 == 0 && file != 0)
+                            win += betAmount * 1;
+                        break;
+                    case 42:
+                        if (redNumbers.Contains(file))
+                            win += betAmount * 1;
+                        break;
+                    case 43:
+                        if (blackNumbers.Contains(file))
+                            win += betAmount * 1;
+                        break;
+                    case 44:
+                        if (file % 2 != 0)
+                            win += betAmount * 1;
+                        break;
+                    case 45:
+                        if (file >= 19 && file <= 36)
+                            win += betAmount * 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return win;
+        }
     }
 }
