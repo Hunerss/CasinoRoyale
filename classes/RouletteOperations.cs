c8bac9c3a5a7ede3d097e890e91d8fa920c5ed5d
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-05-19 16:26:22 +0200

    Refactored classes

--- classes/RouletteOperations.cs
+++ classes/RouletteOperations.cs
@@ -3,16 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Threading;
 
 namespace CasinoRoyale.classes
 {
-    internal class SlotMachineOperator
+    class RouletteOperations
     {
-
-        public SlotMachineOperator()
-        {
-        }
     }
 }
