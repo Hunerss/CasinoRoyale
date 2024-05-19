d9b37309ac7039894b5a81dd331885fe2d37d1f4
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-05-19 16:23:11 +0200

    Created Main class for all operations

---
+++ classes/Operations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CasinoRoyale.classes
+{
+    abstract class Operations
+    {
+        protected int bet = 0;
+        protected readonly Random rnd = new();
+
+        public void SetBet(int bet)
+        {
+            this.bet = bet;
+        }
+
+        public int GetBet()
+        {
+            return this.bet;
+        }
+    }
+}
